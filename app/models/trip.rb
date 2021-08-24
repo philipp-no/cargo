c048bc2fb001c86fc1540bd23124a1c5d456cd51
JenniferGlassmyer <jennifer.glassmyer@gmail.com>
2021-08-24 11:29:49 +0200

    validations on models and create base controllers

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,11 @@ class Trip < ApplicationRecord
   has_many :bookings
   belongs_to :user
   enum status: [:unbooked, :booked, :completed]
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :start_location, presence: true
+  validates :end_location, presence: true
+  validates :price, presence: true
+  validates :capacity, presence: true
+  validates :status, presence: true
 end
