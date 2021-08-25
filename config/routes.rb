400003fe0f23e8d6a56d340fb6ed9522c85ddb24
Otmar Jenner <o.jenner@gmx.de>
2021-08-25 12:11:45 +0200

    Backend implemenbed for: As a driver i see booking requests by senders on a specific trip

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'trips#index'
   resources :trips, only: [:index, :show] do
-    resources :bookings, only: [:new, :create]
-  end 
+    resources :bookings, only: [:new, :create, :index]
+  end
 end
