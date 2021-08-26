fdc7bcdfbcb3430dd6cee9405b1c34b5f6e32571
Philipp <p.nordmeyer@zeppelin-university.net>
2021-08-26 17:51:55 +0200

    accept and decline button works

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,4 +11,16 @@ class BookingsController < ApplicationController
     @trip = Trip.find(params[:trip_id])
     @bookings = @trip.bookings
   end
+
+  def accept
+    @booking = Booking.find_by(trip_id: params[:trip_id])
+    @booking.accepted!
+    redirect_to trip_bookings_path(params[:trip_id]), notice: 'Booking confirmed!'
+  end
+
+  def decline
+    @booking = Booking.find_by(trip_id: params[:trip_id])
+    @booking.declined!
+    redirect_to trip_bookings_path(params[:trip_id]), notice: 'Booking declined!'
+  end
 end
