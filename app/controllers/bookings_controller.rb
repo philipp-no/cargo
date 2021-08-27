3c8bfc3140ba82d54245028ce34db102a0771f35
JenniferGlassmyer <jennifer.glassmyer@gmail.com>
2021-08-27 16:08:03 +0200

    create new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,6 +5,17 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @trip =Trip.find(params[:trip_id])
+    @booking = Booking.new(booking_params)
+    @booking.size = params[:booking][:size][0]
+    @booking.user = current_user
+    @booking.trip = @trip
+    @booking.pending!
+    if @booking.save
+      redirect_to dashboard_index_path, notice: 'Your requested was received and you will be notified when the driver accepts!'
+    else
+      render :new
+    end
   end
 
   def index
@@ -22,5 +33,11 @@ class BookingsController < ApplicationController
     @booking = Booking.find_by(trip_id: params[:trip_id])
     @booking.declined!
     redirect_to trip_bookings_path(params[:trip_id]), notice: 'Booking declined!'
+  end
+
+  private 
+
+  def booking_params
+    params.require(:booking).permit(:description, :size, :item)
   end
 end
