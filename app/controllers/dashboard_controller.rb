be157e343053e0b7e02e3d2426ac02823b039fb1
JenniferGlassmyer <jennifer.glassmyer@gmail.com>
2021-08-29 11:33:52 +0200

    added logic to dashboard to show past bookings and trips in seperate div

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,8 @@
 class DashboardController < ApplicationController
   def index
-    @trips = Trip.where(user: current_user)
-    @bookings = Booking.where(user: current_user)
-  end
-
-  def show
-
+    @trips = Trip.where(user: current_user).where.not(status: 2)
+    @past_trips = Trip.where(user: current_user, status: 2)
+    @bookings = Booking.where(user: current_user).where.not(status: 2)
+    @past_bookings = Booking.where(user: current_user, status: 2)
   end
 end
