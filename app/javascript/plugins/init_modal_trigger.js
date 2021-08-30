eec6f2694dd025ae224107bfe6c29e3846f0628c
JenniferGlassmyer <jennifer.glassmyer@gmail.com>
2021-08-30 16:25:49 +0200

    close model on click outside

--- app/javascript/plugins/init_modal_trigger.js
+++ app/javascript/plugins/init_modal_trigger.js
@@ -6,7 +6,7 @@ const initModalTrigger = () => {
     let buttonsData = button.dataset.tracker;
     modals[buttonsData].classList.toggle("modal-hidden")
     modals[buttonsData].classList.toggle("modal-open")
-    closeModalOnClickOutside();
+    closeModalOnClickOutside(buttonsData);
   }
   if (buttons) {
     buttons.forEach((button) => {
@@ -16,15 +16,13 @@ const initModalTrigger = () => {
     })
   }
 
-  const closeModalOnClickOutside = () => {
-    const modalOpen = document.querySelector(".modal-open")
-    let numberOfClick = 0
-         if (modalOpen) {
-          const modalInner = document.querySelector(".modal-open > .modal-inner")
-          document.addEventListener('click', (e) => { 
-           let isClickInside = modalInner.contains(e.target);
-           numberOfClick +=1
-           if (!isClickInside && numberOfClick > 1) {
+  const closeModalOnClickOutside = (index) => {
+    let modalOpen = document.querySelector(`.modal-${index}.modal-open`)
+    if (modalOpen) {
+      let modalInner = document.querySelector(`.modal-${index}>.modal-inner`)
+      modalOpen.addEventListener('click', (e) => { 
+        let isClickInside = modalInner.contains(e.target);
+           if (!isClickInside) {
              modalOpen.classList.remove("modal-open")
              modalOpen.classList.add('modal-hidden');
            }
