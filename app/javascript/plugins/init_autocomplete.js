bfeed74759555433d1ba98d8e06b61c735a52304
Marie Hasselberg <m.hasselberg@posteo.de>
2021-08-30 16:07:28 +0200

    Worked on navbar, footer, search bar and index page

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -5,17 +5,15 @@ const initAutocomplete = () => {
     const addressInput = document.getElementById('address_input');
     places({ container: addressInput });
   }
-  else if (window.location.pathname == "/trips/new") { 
+  else if (window.location.pathname == "/trips/new") {
   const addressInputs = [document.getElementById('trip_start_location'), document.getElementById('trip_end_location')];
   addressInputs.forEach((addressInput) => {
-    console.log(addressInput);
     places({ container: addressInput });
   });
   }
   else if (window.location.pathname == "/") {
     const searchAddressInputs = [document.getElementById('start_location'), document.getElementById('end_location')];
     searchAddressInputs.forEach((addressInput) => {
-      console.log(addressInput);
       places({ container: addressInput });
     });
   };
