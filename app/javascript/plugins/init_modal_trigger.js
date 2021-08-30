28939f360ee5b43ff4abb3b1e57ac34d11633660
JenniferGlassmyer <jennifer.glassmyer@gmail.com>
2021-08-30 15:34:28 +0200

    close  model on click outside

--- app/javascript/plugins/init_modal_trigger.js
+++ app/javascript/plugins/init_modal_trigger.js
@@ -1,9 +1,12 @@
 const initModalTrigger = () => {
   const buttons = document.querySelectorAll('#modal-trigger-button')
   const modals = document.querySelectorAll('#accept-modal')
+
   const modalTrigger = (button) => {
     let buttonsData = button.dataset.tracker;
     modals[buttonsData].classList.toggle("modal-hidden")
+    modals[buttonsData].classList.toggle("modal-open")
+    closeModalOnClickOutside();
   }
   if (buttons) {
     buttons.forEach((button) => {
@@ -11,6 +14,22 @@ const initModalTrigger = () => {
         modalTrigger(button)
       }, false) 
     })
+  }
+
+  const closeModalOnClickOutside = () => {
+    const modalOpen = document.querySelector(".modal-open")
+    let numberOfClick = 0
+         if (modalOpen) {
+          const modalInner = document.querySelector(".modal-open > .modal-inner")
+          document.addEventListener('click', (e) => { 
+           let isClickInside = modalInner.contains(e.target);
+           numberOfClick +=1
+           if (!isClickInside && numberOfClick > 1) {
+             modalOpen.classList.remove("modal-open")
+             modalOpen.classList.add('modal-hidden');
+           }
+         })
+       }
   }
 };
 
