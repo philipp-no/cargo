8946d5cdde1606aa4a5fd08b3f4e229b2a98bbce
JenniferGlassmyer <jennifer.glassmyer@gmail.com>
2021-09-02 12:17:21 +0200

    style booking new page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,7 +15,7 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to dashboard_index_path, notice: 'Your request was received and you will be notified when the driver accepts!'
     else
-      render :new 
+      render :new, notice: 'Size is required' 
     end
   end
 
