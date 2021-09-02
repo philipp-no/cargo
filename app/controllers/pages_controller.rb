f882799cf6f4b76aca88a23d1ea0fa152f1bb2d9
JenniferGlassmyer <jennifer.glassmyer@gmail.com>
2021-09-02 10:31:07 +0200

    created route through pages controller for about us page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
-  def home
+  skip_before_action :authenticate_user!, only: [:about]
+  def about
   end
 end
